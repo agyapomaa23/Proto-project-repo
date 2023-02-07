236e45852e7fc6c1715ce9a0a0d250b072036945
AGYAPOMAA <agyapomaa23@gmail.com>
2023-02-07 21:01:33 +0000

    protoproject-terrafrom

---
+++ variable.tf
@@ -0,0 +1,30 @@
+# vpc variables
+variable "vpc_cidr" {
+    default          = "10.0.0.0/16" 
+    description      = "vpc cidr block"
+    type             = string
+}
+
+variable "public_subnet_az1_cidr" {
+    default          = "10.0.0.0/24" 
+    description      = "public subnet az1 cidr block"
+    type             = string
+}
+
+variable "public_subnet_az2_cidr" {
+    default          = "10.0.1.0/24" 
+    description      = "public subnet az2 cidr block"
+    type             = string
+}
+
+variable "private_subnet_az1_cidr" {
+    default          = "10.0.2.0/24" 
+    description      = "private subnet az1 cidr block"
+    type             = string
+}
+
+variable "private_subnet_az2_cidr" {
+    default          = "10.0.3.0/24" 
+    description      = "private subnet az2 cidr block"
+    type             = string
+}
